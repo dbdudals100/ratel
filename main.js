95b2e8d85bfe9086ee95d95066da42a581da065d
dbdudals100 <88155610+dbdudals100@users.noreply.github.com>
2021-12-14 12:54:04 +0900

    Javascript : togglebtn

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ if(window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
 } else navbar.classList.remove('navbar--dark');
 });
+
+
+const btn = document.querySelector('.navbar__toggle-btn');
+const navbarmenu = document.querySelector('.navbar__menu');
+btn.addEventListener('click',()=>{
+ if(!navbarmenu.classList.contains('navbar--btn')){
+     navbarmenu.classList.add('navbar--btn');
+ }else{
+     navbarmenu.classList.remove('navbar--btn');
+ };
+});
