8a12a9752bd6ddbd01f3f142450197bffcbdfe29
dbdudals100 <88155610+dbdudals100@users.noreply.github.com>
2021-12-15 22:44:29 +0900

    Js : home scroll opacity

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 'use strict'
 
-//navbar scroll 
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
+//navbar scroll , 아래 두개의 코멘트는 navbar의 scroll을 구하는 것
+
+//const navbar = document.querySelector('#navbar');
+//const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll',()=>{
-if(window.scrollY > navbarHeight) {
+if(window.scrollY > 100) {
     navbar.classList.add('navbar--dark');
 } else navbar.classList.remove('navbar--dark');
 });
@@ -20,8 +21,8 @@ btn.addEventListener('click',()=>{
  };
 });
 
+//navbar menu click to id scroll 
 const navbarclick = document.querySelector('.navbar__menu')
-
 document.addEventListener('click',(event)=>{
     const target = event.target
     const link = target.dataset.link
@@ -32,3 +33,12 @@ document.addEventListener('click',(event)=>{
      scrollto.scrollIntoView({behavior: "smooth" });
     }
 })
+
+const homeopacity = document.querySelector('.home__container')
+const homeHeigth = homeopacity.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+   homeopacity.style.opacity = 1 -  window.scrollY/homeHeigth; 
+});
+
+
+//  250 380
